a7c7c86524448913eb442fd612eb6e29d831aaba
jgonzalez2020344 <jgonzalez-2020344@kinal.edu.gt>
2022-05-19 00:18:41 -0600

    Login
    Daniel González

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const usuarioControlador = require('../controllers/usuario.controller');
-const md_autenticacion = require('../middlewares/autenticacion');
+const md_rol = require('../middlewares/rol');
 
 const api = express.Router();
 
-api.post('/login', usuarioControlador.Login);// Login de Administrador y de Clientes
+api.post('/login' ,usuarioControlador.Login);// Login de Administrador y de Clientes
 
 module.exports = api;
